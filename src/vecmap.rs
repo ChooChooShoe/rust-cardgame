75b28846553a1de7170975cbec126232a578a24c
Timothy Durbano <timothy.durbano@gmail.com>
2018-09-15 18:36:25 -0400

    New working VecMap.

--- src/vecmap.rs
+++ src/vecmap.rs
@@ -1,28 +1,79 @@
-pub struct VecMap<V> {
-    inner: Vec<Option<V>>,
+//VecMap<T> sors data in a Vec<T> but sets and gets like a HashMap<usize,T>.
+
+// See also: Generational indexes
+// https://kyren.github.io/2018/09/14/rustconf-talk.html
+
+use std::num::NonZeroU32;
+
+#[derive(Eq, PartialEq, Copy, Clone, Debug)]
+pub struct IndexKey {
+    index: u32,
+    generation: NonZeroU32,
+}
+
+impl IndexKey {
+    pub fn new(index: u32, generation: u32) -> IndexKey {
+        IndexKey {
+            index,
+            generation: NonZeroU32::new(generation).unwrap(),
+        }
+    }
+    pub fn from_nonzero(index: u32, generation: NonZeroU32) -> IndexKey {
+        IndexKey { index, generation }
+    }
+    pub fn index(&self) -> usize {
+        self.index as usize
+    }
+    pub fn generation(&self) -> u32 {
+        self.generation.get()
+    }
+}
+
+struct Entry<T> {
+    generation: NonZeroU32,
+    value: T,
+}
+impl<T> Entry<T> {
+    // Creates new entry. generation must not be 0.
+    fn new(value: T, generation: NonZeroU32) -> Entry<T> {
+        Entry { value, generation }
+    }
+    fn first(value: T) -> Entry<T> {
+        Entry { value, generation: NonZeroU32::new(1).unwrap() }
+    }
+
+    fn generation(&self) -> u32 {
+        self.generation.get()
+    }
+}
+
+pub struct VecMap<T> {
+    inner: Vec<Option<Entry<T>>>,
+    free_head: usize,
     count: usize,
 }
 
-impl<V> VecMap<V> {
-    pub fn new() -> VecMap<V> {
-        VecMap {
-            inner: Vec::new(),
-            count: 0,
-        }
+impl<T> VecMap<T> {
+    pub fn new() -> VecMap<T> {
+        Self::with_capacity(0)
     }
-    pub fn with_capacity(capacity: usize) -> VecMap<V> {
+    pub fn with_capacity(capacity: usize) -> VecMap<T> {
         VecMap {
             inner: Vec::with_capacity(capacity),
+            free_head: 0,
             count: 0,
         }
     }
-    pub fn from_vec(vec: Vec<V>) -> VecMap<V> {
-        let count = vec.len();
-        let mut inner = Vec::with_capacity(count);
+    pub fn from_vec(vec: Vec<T>) -> VecMap<T> {
+        let mut inner = Vec::with_capacity(vec.len());
         for x in vec.into_iter() {
-            inner.push(Some(x));
+            inner.push(Some(Entry::first(x)));
         }
-        VecMap { inner, count }
+        VecMap {
+            inner,
+            free_head: 0,
+            count: 0,
+        }
     }
     pub fn capacity(&self) -> usize {
         self.inner.capacity()
@@ -46,43 +97,57 @@ impl<V> VecMap<V> {
         self.inner.clear()
     }
 
-    /// Returns a reference to the value corresponding to the index.
-    pub fn get(&self, index: usize) -> Option<&V> {
-        match self.inner.get(index) {
-            None => None,                  // index out of bounds
-            Some(&None) => None,           // inboud, but no value set
-            Some(&Some(ref v)) => Some(v), // inboud and set, return Some(ref)
+    // Returns a reference to the value corresponding to the generational index.
+    // The generation must match.
+    pub fn get(&self, index: IndexKey) -> Option<&T> {
+        match self.inner.get(index.index()) {
+            None => None,       // index out of bounds
+            Some(None) => None, // inbound, but no value set
+            Some(Some(entry)) => {
+                // inbound and set, return Some(value) if generations match.
+                if entry.generation() == index.generation() {
+                    Some(&entry.value)
+                } else {
+                    None
+                }
+            }
         }
     }
 
-    /// Will not panic if index is out of bounds
-    pub fn insert(&mut self, index: usize, element: V) -> Option<V> {
-        if index >= self.len() {
-            for _ in 0..(self.len() - index) {
+    pub fn get_mut(&mut self, index: IndexKey) -> Option<&mut T> {
+        match self.inner.get_mut(index.index()) {
+            None => None,       // index out of bounds
+            Some(None) => None, // inbound, but no value set
+            Some(Some(entry)) => {
+                // inbound and set, return Some(value) if generations match.
+                if entry.generation() == index.generation() {
+                    Some(&mut entry.value)
+                } else {
+                    None
+                }
+            }
+        }
+    }
+
+    // Set the value for some generational index.  May overwrite past generation
+    // values.
+    pub fn set(&mut self, index: IndexKey, value: T) {
+        if index.index() < self.len() {
+            self.inner[index.index()] = Some(Entry::new(value, index.generation))
+        } else {
+            for _ in 0..(self.len() - index.index()) {
                 self.inner.push(None)
             }
-            self.inner.push(Some(element));
-            None
-        } else {
-            self.inner.insert(index, Some(element));
-            None
+            assert_eq!(self.len(), index.index());
+            self.inner.push(Some(Entry::new(value, index.generation)))
         }
     }
-    /// Removes at index from the VecMap, returning the value at the  index.
-    pub fn remove(&mut self, index: usize) -> Option<V> {
-        if index >= self.len() {
-            None
-        } else {
-            self.inner.remove(index)
-        }
+
+
+    pub fn insert(&mut self, value: T) -> IndexKey
+    {
+        let key = IndexKey::new(self.len() as u32, 1);
+        self.set(key,value);
+        key
     }
-    /// Appends an element to the back of a collection.
-    pub fn push(&mut self, value: V) {
-        self.inner.push(Some(value))
-    }
-    pub fn iter(&self) -> Iter<Option<V>> {
-        self.inner.iter()
-    }
-    // TODO: the other map and vac functions.
 }
-use std::slice::Iter;
